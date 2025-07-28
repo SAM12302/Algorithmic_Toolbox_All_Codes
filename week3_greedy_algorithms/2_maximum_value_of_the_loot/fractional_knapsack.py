95bfd599669ab8bfd777197bbeb26e9b04f928e8
SAM12302 <sameer.younas83@gmail.com>
2025-07-28 19:44:49 +0500

    1. knapsack problem solved

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -2,16 +2,41 @@ from sys import stdin
 
 
 def optimal_value(capacity, weights, values):
-    value = 0.
+    value = 0.0
+    weight = 0.0
+    goodness_list = []
     # write your code here
+    """n = 4, capacity = 50
+    item1: value = 60, weight = 10
+    item2: value = 100, weight = 20
+    item3: value = 120, weight = 30"""
+    for v, w in zip(values, weights):
+        goodness = v/w
+        goodness_list.append((goodness, v, w))
+
+    goodness_list.sort(reverse=True)
+    for goodness, v, w in goodness_list:
+        if weight + w <= capacity:
+            value = value + v
+            weight = weight + w
+        else:
+            remaining_capacity = capacity - weight
+            value = value + goodness * remaining_capacity
+            break          
+
+    print(goodness_list)
 
     return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, stdin.read().split()))
-    n, capacity = data[0:2]
+    #data = list(map(int, stdin.read().split()))
+    """    n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
+    weights = data[3:(2 * n + 2):2]"""
+    n = 3  # No more than 3 items
+    capacity = 50  # Example knapsack capacity
+    values = [60, 100, 120]  # Example item values
+    weights = [10, 20, 30]  # Example item weights
     opt_value = optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
