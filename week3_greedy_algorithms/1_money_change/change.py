e44afef20b6f128c03c977bb06ee94c35bddbc4a
SAM12302 <sameer.younas83@gmail.com>
2025-07-28 18:41:29 +0500

    1.  changed money_change

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,7 +1,25 @@
 def change(money):
-    # write your code here
+    count_10 = 0
+    count_5 = 0
+    count_1 = 0
 
-    return money
+    while money != 0:
+        if money >= 10:
+            money -= 10
+            count_10 += 1
+        elif money >= 5:
+            money -= 5
+            count_5 += 1
+        else:
+            money -= 1
+            count_1 += 1
+
+    parts = []
+    parts.extend(["10"] * count_10)
+    parts.extend(["5"] * count_5)
+    parts.extend(["1"] * count_1)
+
+    return " + ".join(parts)
 
 
 if __name__ == '__main__':
