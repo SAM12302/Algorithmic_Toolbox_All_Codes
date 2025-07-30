43a9933346cd50341691489670939e489cb3a9f7
SAM12302 <sameer.younas83@gmail.com>
2025-07-30 17:26:21 +0500

    1. not greedy but within the time constraint

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,8 +1,18 @@
+result = []
 def fibonacci_number(n):
-    if n <= 1:
-        return n
-
-    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+    result.append(0)
+    result.append(1)
+    if n < 1:
+        return result[0]
+    elif n <= 1:
+        return result[1]
+    else:
+        count = result.index(1)
+        while count < n:
+            next_fib = result[count] + result[count - 1]
+            count = count + 1
+            result.append(next_fib)
+        return result[count]
 
 
 if __name__ == '__main__':
