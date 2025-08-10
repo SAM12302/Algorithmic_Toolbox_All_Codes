9052468eea087faf94bebfb5170331867b5fc471
SAM12302 <sameer.younas83@gmail.com>
2025-08-10 17:19:21 +0500

    1. my logic

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,7 +1,18 @@
 def optimal_summands(n):
     summands = []
     # write your code here
-    return summands
+    i = 1
+    remaining = n
+    """    while i <= n:
+        remaining = remaining - i
+        if remaining < i:
+            summands.append(remaining + i)
+            break
+        else:
+            summands.append(i)
+        i = i + 1
+    return summands"""
+    #changing logic, close but fails test cases
 
 
 if __name__ == '__main__':
